ef7b86d4751610c7c6fd330d4cbc039880215ddb
felixpangestu <felixpangestufsp@gmail.com>
2022-01-26 12:55:18 +0000

    Basic polynomial implementation
    A polynomial class which knows its degree and cna be converted to a string.

---
+++ polynomials/polynomials.py
@@ -0,0 +1,27 @@
+class Polynomial:
+    def __init__(self, coefs):
+
+        self.coefficients = coefs
+
+    def degree(self):
+
+        return len(self.coefficients) - 1   
+
+    def __str__(self):
+
+        coefs = self.coefficients
+        terms = []
+
+        #Degree 0 and 1 terms conventionally have different representation
+        if coefs[0]:
+            terms.append(str(coefs[0]))
+        if self.degree() > 0 and coefs[1]:
+            terms.append(f"{'' if coefs[1] == 1 else coefs[1]}x")
+
+        #Remaining terms look like cx^d, but the factors of 1 are dropped
+        terms += [f"{'' if c == 1 else c}x^{d}"
+                for d, c in enumerate(coefs[2:], start=2) if c]
+
+        #write it in descending order
+        #This returns the second one if the first one is false
+        return " + ".join(reversed(terms)) or "0"
